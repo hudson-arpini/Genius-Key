e69a58cdb9c022582794374271897e9862c25247
Hudson Arpini <hudson_arpini@hotmail.com>
2022-04-01 23:29:13 -0300

    adicionados menus e botões de navegação

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+//FUNÇÕES DO PIANO 
+
 let getKeyCode = (event) => {
     let keyCode
     if(event.type === "keydown"){keyCode = event.keyCode}
@@ -27,7 +29,7 @@ let removePlayingClass = (event) => {
     event.target.classList.remove('playing')
 }
 
-
+//DOM DO PIANO
 
 const keys = document.querySelectorAll(".key")
 
@@ -37,3 +39,54 @@ keys.forEach((key) => {
 })
 
 window.addEventListener("keydown", play)
+
+//DOM DAS TELAS
+
+let telaPrincipal = document.querySelector("#main-screen")
+let piano = document.querySelector("#piano")
+
+let facil = document.querySelector("#facil")
+let medio = document.querySelector("#medio")
+let dificil = document.querySelector("#dificil")
+let livre = document.querySelector("#livre")
+let voltar = document.querySelector("#voltar")
+let start = document.querySelector("#start")
+
+facil.addEventListener("click", ()=>{
+  if (telaPrincipal.style.display === "none") {telaPrincipal.style.display = "flexflex"} 
+  else {telaPrincipal.style.display = "none"}
+    piano.classList.toggle("none")
+    voltar.classList.toggle("none")
+    start.classList.toggle("none")
+})
+
+medio.addEventListener("click", ()=>{
+  if (telaPrincipal.style.display === "none") {telaPrincipal.style.display = "flex"}
+  else {telaPrincipal.style.display = "none"}
+    piano.classList.toggle("none")
+    voltar.classList.toggle("none")
+    start.classList.toggle("none")
+})
+
+dificil.addEventListener("click", ()=>{
+  if (telaPrincipal.style.display === "none") {telaPrincipal.style.display = "flex"}
+  else {telaPrincipal.style.display = "none"}
+    piano.classList.toggle("none")
+    voltar.classList.toggle("none")
+    start.classList.toggle("none")
+})
+
+livre.addEventListener("click", ()=>{
+  if (telaPrincipal.style.display === "none") {telaPrincipal.style.display = "flex"}
+  else {telaPrincipal.style.display = "none";}
+    piano.classList.toggle("none")
+    voltar.classList.toggle("none")
+})
+
+voltar.addEventListener("click", ()=>{
+    if (telaPrincipal.style.display === "none") {telaPrincipal.style.display = "flex"}
+    else {telaPrincipal.style.display = "none";}
+      piano.classList.toggle("none")
+      voltar.classList.toggle("none")
+      start.classList.add("none")
+  })
