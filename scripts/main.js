1f9a0868180b493b12df324dc9f61450471419dd
Hudson Arpini <hudson_arpini@hotmail.com>
2022-04-04 22:21:55 -0300

    Implementada lógica de genius e condição de vitória/derrota

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 //FUNÇÕES DO PIANO 
 
+
+//PEGAR KEYCODE COM EVENTO DE KEYDOWN OU CLICK
 let getKeyCode = (event) => {
     let keyCode
     if(event.type === "keydown"){keyCode = event.keyCode}
@@ -7,27 +9,43 @@ let getKeyCode = (event) => {
     return keyCode
 }
 
+//TOCAR O AUDIO REFERENTE AO KEYCODE
 let audio = (keyCode) => {
     let audio = document.querySelector(`audio[data-key="${keyCode}"]`)
     audio.currentTime = 0
     audio.play()
 }
 
+//ADICIONAR EFEITOS DE CSS
 let addplayingClass = (key) => {
     key.classList.add ("playing")
 }
 
+//REMOVER EFEITOS DE CSS
 let removePlayingClass = (event) => {
   event.target.classList.remove('playing')
 }
 
+
+//FUNÇÃO PRINCIPAL: DISPARAR TODOS OS GATILHOS QUANDO UMA TECLA É ATIVADA
+let jogador =[]
 let play = (event) => {
+  //PEGANDO O KEY CODE
     let keyCode = getKeyCode(event)
     let key = document.querySelector(`[data-key="${keyCode}"]`)
     if(!key){return}
+    //TOCANDO E ADICIONANDO EFEITOS NA TECLA
     audio(keyCode)
     addplayingClass(key)
-}
+    //TESTES DE VITÓRIA OU DERROTA DO GENIUS
+    jogador.push(keyCode)
+    console.log(jogador)
+    if(jogador.length === sequencia.length){
+      if(JSON.stringify(jogador) === JSON.stringify(sequencia)) {window.alert("Você venceu!")}
+      else{window.alert("Que pena, tente novamente")}
+      jogador = []
+    }
+  }
 
 
 //DOM DO PIANO
@@ -97,11 +115,12 @@ voltar.addEventListener("click", ()=>{
 
 start.addEventListener("click",()=>{
     genius(dificuldade)
+    
 })
 
   //FUNÇÕES DO GENIUS
 
-  
+  let sequencia = []
   let genius = (dificuldade) => {
     numTeclas = []
     //CRIANDO UM ARRAY COM OS KEYCODES
@@ -114,7 +133,7 @@ start.addEventListener("click",()=>{
       [numTeclas[i], numTeclas[j]] = [numTeclas[j], numTeclas[i]];
     }
     //DEFININDO O TAMANHO DO ARRAY DE ACORDO COM A DIFICULDADE
-    let sequencia = numTeclas.slice(0,dificuldade)
+    sequencia = numTeclas.slice(0,dificuldade)
     //TOCANDO AS NOTAS DA SEQUENCIA
     let k=0
     let id = setInterval(()=>{
@@ -123,6 +142,12 @@ start.addEventListener("click",()=>{
         k++
         if(k === dificuldade){clearInterval(id)}
       },1500)
+      console.log(sequencia)
   }
-
   
+
+
+  /*FUNÇÕES DO JOGADOR
+    let teste = () => {
+    if(JSON.stringify(jogador) == JSON.stringify(sequencia)) {window.alert("Você venceu!")}
+    }*/
